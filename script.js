499bfffd0c07757a9942c9f24249d2f722772a68
Gustave Curtil <gustave.curtil@tutanota.com>
2024-06-21 11:07:14 +0200

    layout herorganiseren

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
 let tabs = document.querySelectorAll("nav ul li");
 let pages = document.querySelectorAll("main");
-let title = document.querySelector('.title h2');
 
 for (let i = 0; i < tabs.length; i++) {
     tabs[i].addEventListener('click', (e) => {
-        if (i===0) {
-            title.innerHTML = "";
-        } else {
-            title.innerHTML = tabs[i].innerHTML;
-        }
         tabs.forEach(tab => {
             tab.classList.remove('current');
         });
